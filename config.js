6e38df2717f92c01881f3335477a6a4cad097680
koyeb01125 <apnapremiummarket011@gmail.com>
2025-02-19 01:57:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     domains: ["lamoshitt.com", "crazzybabe.com"], // names of domain for the emails. 
     oneTimeMailGen: 10 * 100000,
     telegramBotToken: "7710565909:AAEQ7LZx_2x_W4m0NOddnJ-W2hVIRIdSjJQ", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
-    adminNamesForTelegramBot: ["aarav_0008","hahaha7726"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
-    authorisedUserForTelegramBot: ["aarav_0008","hahaha7726"] // username or id of the user who can use the bot.
+    adminNamesForTelegramBot: ["Adityaseller02"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+    authorisedUserForTelegramBot: ["Adityaseller02"] // username or id of the user who can use the bot.
 };
